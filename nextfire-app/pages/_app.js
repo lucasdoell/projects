6527d1ac3b0cfd9e0f0de9b05d226d152d096610
Lucas Doell <lmd248@cornell.edu>
2022-07-16 16:24:05 -0400

    added navbar and toast support

--- nextfire-app/pages/_app.js
+++ nextfire-app/pages/_app.js
@@ -1,7 +1,17 @@
+import Navbar from '../components/Navbar';
 import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    
+    </>
+  );
+  
 }
 
 export default MyApp
